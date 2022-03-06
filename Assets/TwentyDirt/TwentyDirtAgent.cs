409d903b2ce290d5af4f5615c8016d561ff184df
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-03-05 16:40:00 -0800

    cleaner code

--- Assets/TwentyDirt/TwentyDirtAgent.cs
+++ Assets/TwentyDirt/TwentyDirtAgent.cs
@@ -8,120 +8,49 @@ using Unity.MLAgents.Actuators;
 
 public class TwentyDirtAgent : Agent
 {
-    /*
-    if agents hits dirt, dirt disappears
-    random spawn of dirt
-    if agents moves to far away respawn adjustments
-    */
+    //couter for later
     public int counter = 0;
 
-    [Tooltip("Move speed in meters/second")]
-    public float moveSpeed = 10f;
-    [Tooltip("Turn speed in degrees/second, left (+) or right (-)")]
+    //movespeed of agent
+    public float moveSpeed = 3f;
+
+    //turnspeed of agent
     public float turnSpeed = 300;
 
-    [Tooltip("The platform to be moved around")]
-    public GameObject goal;
-    //ich kann für jedes gameobject ein einzelnes objekt machen
-
-    public GameObject goal2;
-
-    public GameObject goal3;
-
-    public GameObject goal4;
-
-    public GameObject goal5;
-
-    public GameObject goal6;
-
-    public GameObject goal7;
-
-    public GameObject goal8;
-
-    public GameObject goal9;
-
-    public GameObject goal10;
-
-    public GameObject goal11;
-
-    public GameObject goal12;
-
-    public GameObject goal13;
-
-    public GameObject goal14;
-
-    public GameObject goal15;
-
-    public GameObject goal16;
-
-    public GameObject goal17;
-
-    public GameObject goal18;
-
-    public GameObject goal19;
-
-    public GameObject goal20;
-
+    //vector to which the start position is passed later
     private Vector3 startPosition;
-    //private SimpleCharacterController characterController;
+
     new private Rigidbody rigidbody;
 
     public override void Initialize()
     {
         startPosition = transform.position;
-        //characterController = GetComponent<SimpleCharacterController>();
+
+        //get rigidbody of agent
         rigidbody = GetComponent<Rigidbody>();
     }
-
+    
+    //init dirt objects. SerializeField for accessing dirt objects through Unity Editor
+    [SerializeField] private GameObject[] goals = new GameObject[20];
+    
     public override void OnEpisodeBegin()
     {
+        for(int i = 0; i < goals.Length; i++){ 
+            goals[i].SetActive(true);
+            /*
+            Quaternion = used to represent rotations 
+            Quaternion.Euler = returns rotation that rotates z degrees around the x axis and y degrees around the y axis; applied in that order
+            Vektor3.up = shorthand for Vector3(0, 1, 0)
+            Vektor3.forward =  shorthand for Vector3(0, 0, 1)
+
+            */
+            goals[i].transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * Random.Range(5f, 20f);
+        }
+
         // Reset agent position, rotation
         transform.position = startPosition;
         transform.rotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
         rigidbody.velocity = Vector3.zero;
-
-        goal.SetActive(true);
-        goal2.SetActive(true);
-        goal3.SetActive(true);
-        goal4.SetActive(true);
-        goal5.SetActive(true);
-        goal6.SetActive(true);
-        goal7.SetActive(true);
-        goal8.SetActive(true);
-        goal9.SetActive(true);
-        goal10.SetActive(true);
-        goal11.SetActive(true);
-        goal12.SetActive(true);
-        goal13.SetActive(true);
-        goal14.SetActive(true);
-        goal15.SetActive(true);
-        goal16.SetActive(true);
-        goal17.SetActive(true);
-        goal18.SetActive(true);
-        goal19.SetActive(true);
-        goal20.SetActive(true);
-
-        // Reset platform position (5 meters away from the agent in a random direction)
-        goal.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f;
-        goal2.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 10f;
-        goal3.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 15f;
-        goal4.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f;
-        goal5.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 10f;
-        goal6.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 15f;
-        goal7.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f;
-        goal8.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 10f;
-        goal9.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 15f;
-        goal10.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f;
-        goal11.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 10f;
-        goal12.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 15f;
-        goal13.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f;
-        goal14.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 10f;
-        goal15.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 15f;
-        goal16.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f;
-        goal17.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 10f;
-        goal18.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 15f;
-        goal19.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f;
-        goal20.transform.position = startPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 10f;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -154,10 +83,6 @@ public class TwentyDirtAgent : Agent
         float horizontal = actions.DiscreteActions[1] <= 1 ? actions.DiscreteActions[1] : -1;
         bool jump = actions.DiscreteActions[2] > 0;
 
-        /*characterController.ForwardInput = vertical;
-        characterController.TurnInput = horizontal;
-        characterController.JumpInput = jump;*/
-
         // Turning
         if (horizontal != 0f)
         {
@@ -171,15 +96,10 @@ public class TwentyDirtAgent : Agent
         rigidbody.MovePosition(transform.position + move);
     }
 
-    
-
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log(other);
-
         if (other.gameObject.tag == "goal")
         {
-            //Destroy(other.gameObject);
             other.gameObject.SetActive(false);
             counter += 1;
             AddReward(1f);
@@ -188,6 +108,5 @@ public class TwentyDirtAgent : Agent
                 EndEpisode();
             }
         }
-        // If the other object is a collectible, reward and end episode
     }
 }
